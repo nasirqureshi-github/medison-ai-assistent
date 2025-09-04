cc5d83fe037ac9df1891cf90458a345d189c509c
Nasir qureshi <138995290+nasirqureshi-github@users.noreply.github.com>
2025-09-05 03:06:18 +0500

    update page

--- src/app/sections/About.js
+++ src/app/sections/About.js
@@ -4,7 +4,7 @@ const About = () => {
   return (
     <div className="container mx-auto pt-30" id='about'>
       <h1 className="text-center text-[32px] font-bold">How It Works</h1>
-      <h2 className="text-center text-lg">Geeting started with Madison is a simple and straightforward. Here Your jouney to better health</h2>
+      <h2 className="text-center text-lg">Geeting started with Madison is a simple and a straightforward. Here Your jouney to better health</h2>
         <Aboutcard/> 
     </div>
 
